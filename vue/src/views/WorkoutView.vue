dfb7b65b44589812261f5bf213ab76bcbae548be
Nikolas <nknappen@gmail.com>
2024-08-07 12:07:53 -0400

    12:07 Exercise Frontend

--- vue/src/views/WorkoutView.vue
+++ vue/src/views/WorkoutView.vue
@@ -1,14 +1,52 @@
 <template>
     <h2>Workout</h2>
-    <div></div>
+    <div>
+        <table id="exercises">
+            <thead>
+                <tr>
+                    Exercise
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>
+                        <input type="text" id="exerciseName" v-model="exerciseName">
+                    </td>
+                </tr>
+                <tr v-for="exercise in filteredExercises" :key="exercise.exerciseId">
+                    <td>{{exercise.exerciseName}}</td>
+                    <td>
+                        <div v-show="selectedExerciseId === exercise.exerciseId">
+                            gif goes here
+                            {{ exercise.description }}
+                        </div>
+                    </td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
     <button id="check-out" v-on:click="checkOut">Complete Workout/check-out</button>
 </template>
 <script>
 import CheckInCheckOutService from '../services/CheckInCheckOutService';
+import ExerciseServices from '../services/ExerciseServices';
 export default {
     data() {
         return {
-            user: JSON.parse(localStorage.getItem('user'))
+            user: JSON.parse(localStorage.getItem('user')),
+            exercises: [],
+            exerciseName: '',
+        };
+    },
+    computed: {
+        filteredExercises() {
+            let filteredExercises = this.exercises;
+            if (this.exerciseName) {
+                filteredExercises = filteredExercises.filter((exercise) => exercise.exerciseName
+                    .toLowerCase()
+                    .includes(this.exerciseName));
+            }
+            return filteredExercises;
         }
     },
        methods: {
@@ -24,7 +62,24 @@ export default {
                 .catch((error) => {
                     console.log(error);
                 });  
+        },
+        getExercises() {
+            ExerciseServices.getExercises()
+                .then((response) => {
+                    this.exercises = response.data;
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
+        selectExercise(exerciseId) {
+            // Toggle the selected exercise ID
+            this.selectedExerciseId = this.selectedExerciseId === exerciseId ? null : exerciseId;   
         }
+    },
+    mounted() {
+        this.getExercises();
     }
+
 }
 </script>
